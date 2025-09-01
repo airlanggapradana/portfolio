102df3c105b6a6633750b848a976f315ae2fa0cf
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-09-01 14:19:29 +0700

    feat: add Vercel Analytics to main.tsx and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router";
+import {Analytics} from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App/>}/>
       </Routes>
     </BrowserRouter>
+    <Analytics/>
   </StrictMode>,
 )
