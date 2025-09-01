a521193869c994ed3d6dd8c69cf465fe4d71afbb
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-09-01 14:14:44 +0700

    refactor: remove HelmetProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router";
-import {HelmetProvider} from "react-helmet-async";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App/>}/>
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
