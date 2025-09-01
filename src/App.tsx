dfb66593044b3c752936aa511528a5a443ee0047
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-09-01 14:13:18 +0700

    refactor: remove SEO functionality and related dependencies from the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,9 @@ import Navbar from "@/components/Navbar.tsx";
 import Hero from "@/components/Hero.tsx";
 import Works from "@/components/Works.tsx";
 import Contact from "@/components/Contacts.tsx";
-import useSEO from "@/hooks/useSeo.tsx";
 
 function App() {
   useLenis();
-  useSEO({
-    title: "Home - My Portfolio Website",
-    description: "Welcome to my website, here you can find amazing content.",
-    keywords: "portfolio, web development, projects, airlangga",
-    image: "https://example.com/thumbnail.png",
-    url: "https://mywebsite.com/",
-  })
   return (
     <main
       className={'w-full min-h-screen bg-gray-100 flex flex-col items-center justify-center text-2xl px-4 sm:px-6 lg:px-8'}>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-import ViteSitemapPlugin from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-    ViteSitemapPlugin({
-      hostname: "https://mywebsite.com", // ganti dengan domain kamu
-    }),],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
